b40e7729e4612c98d08a864174ccaa654e961a76
EmilieGriffin22 <lelag040804@gmail.com>
2024-01-08 17:05:57 -0500

    Attempt to fix TypeError.

--- script.js
+++ script.js
@@ -4,43 +4,54 @@
 //{'Type of Crime' : {'longitude' : list, 'latitude' : list, 'labels' : list}, etc}
 //It then should create a PlotyPlot with the data dictionaries seperated by crime so they can be color-coded!
 
-async function generateBetterMapBox(dataJSON){
-    let data = await plotlyDataGenerator(14);
-    let plotlyList = []
-    //TODO: Fix issue with data being a promise (this function is asynchonous).
-    for (let key of data) {
-        let currDic = data[key]
-        let dataDic = {
-            type : 'scattermapbox',
-            lat : currDic['latitude'],
-            lon : currDic['longitude'],
-            mode : 'markers',
-            text : currDic['labels'],
-            name : key,
-            marker: {
-                size : 12
+async function generateBetterMapBox(dataJSON) {
+    try {
+        let data = await plotlyDataGenerator(14);
+        let plotlyList = [];
+
+        // Ensure that data is an array
+        if (!Array.isArray(data)) {
+            console.error('Invalid data format. Expected an array.');
+            return;
+        }
+
+        for (let key of data) {
+            let currDic = data[key];
+            let dataDic = {
+                type: 'scattermapbox',
+                lat: currDic['latitude'],
+                lon: currDic['longitude'],
+                mode: 'markers',
+                text: currDic['labels'],
+                name: key,
+                marker: {
+                    size: 12
+                },
+            };
+            plotlyList.push(dataDic);
+        }
+
+        let layout = {
+            autosize: false,
+            width: window.innerWidth - 1,
+            height: window.innerHeight,
+            hovermode: 'closest',
+            mapbox: {
+                bearing: 0,
+                center: {
+                    lat: 42.88,
+                    lon: -78.87
+                },
+                style: 'carto-darkmatter',
+                pitch: 0,
+                zoom: 10
             },
         };
-        plotlyList.push(dataDic)
+
+        Plotly.newPlot('map', plotlyList, layout);
+    } catch (error) {
+        console.error('Error fetching or processing data:', error);
     }
-
-    let layout = {
-        autosize : false,
-        width: window.innerWidth - 1,
-        height: window.innerHeight,
-        hovermode:'closest',
-        mapbox: {
-            bearing:0,
-            center: {
-                lat: 42.88,
-                lon: -78.87
-            },
-            style: 'carto-darkmatter',
-            pitch:0,
-            zoom:10
-        },
-    };
-
-    Plotly.newPlot('map', plotlyList, layout);
 }
+
 
